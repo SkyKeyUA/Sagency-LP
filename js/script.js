4fd884862cbefdf9fb1bab7081b61da275601651
OLEKSANDR VEREMIIENKO <120383111+SkyKeyUA@users.noreply.github.com>
2025-05-06 10:23:17 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -72,6 +72,11 @@ function scrollActions() {
         headerLinks?.forEach((links) => {
           links?.classList.remove('active');
           document.querySelector('header a[href*=' + id + ']').classList.add('active');
+          if (id === 'offer') {
+            redLine.classList.add('arrow-hidden');
+          } else {
+            redLine.classList.remove('arrow-hidden');
+          }
         });
       }
     });
